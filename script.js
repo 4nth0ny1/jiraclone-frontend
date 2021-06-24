cf3469954ae09a8922057bb806589d9cc2812ad8
Anthony Catullo <acatullo4@gmail.com>
2021-06-24 18:02:30 -0500

    fixed overlap of cards and modal

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 const modal = document.querySelector('#modal')
-const card = document.querySelector('.card-body')
+const card = document.querySelectorAll('.card-body')
 const closeModalBtn = document.querySelector('#close-modal-btn')
 const overlay = document.querySelector('#overlay')
 
-// TODO: 3. Create a click event listener for the open-modal-btn that adds the class "open" to the modal
-// BONUS: Also add the class "open" to the overlay
+const cardArray = Array.from(card)
 
-card.addEventListener('click', () => {
-    modal.classList.add('open')
-    overlay.classList.add('open')
-})
-
-// TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
-// BONUS: Also remove the class "open" from the overlay
+cardArray.forEach(card => 
+    card.addEventListener('click', () => {
+        modal.classList.add('open')
+        overlay.classList.add('open')
+}))
 
 closeModalBtn.addEventListener('click', closeModal)
-
-// BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
 
 overlay.addEventListener('click', closeModal)
 
